b5cf7ce1e8638f30cfc64641325b1c801b4d9bd7
Dominik Kanjuh <dominik.kanjuh@outlook.com>
2023-11-18 00:32:05 +0100

    Santa svg done and responsive

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,12 +1,13 @@
-import { Santa, Separator_1 } from '@/components'
+import { Separator_1 } from '@/components'
+import santa from './../assets/santa.svg'
 
 const Home = () => {
   return (
     <div className="flex flex-col items-center">
       <div className="bg-hero-pattern flex h-screen w-screen items-center justify-center">
         <div className="flex flex-col lg:flex-row justify-start lg:items-center gap-8 lg:gap-36">
-          <div className="h-64 w-96 ">
-            <img src={Santa} className="rounded-3xl object-cover h-72 w-full" />
+          <div className="w-[350px] h-[255px] lg:w-[550px] lg:h-[400px] px-4 bg-blue rounded-3xl lg:rounded-[60px] flex justify-center items-center shadow-xl">
+            <img src={santa} className="w-full h-auto" alt="Slika djeda božičnjaka" />
           </div>
 
           <div className="font-monomaniac flex flex-col text-red">
