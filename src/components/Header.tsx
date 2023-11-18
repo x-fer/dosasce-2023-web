66f9b101d7015d0fa539c62cd5b109e1241fa1d3
Dominik Kanjuh <dominik.kanjuh@outlook.com>
2023-11-18 21:00:31 +0100

    quick fix dep

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,12 @@
 import { GoogleLogin } from '@react-oauth/google'
 
 const Header = () => {
-  const onSuccess = (response: any) => {
+  const onSuccess = (response: unknown) => {
     console.log(response)
-    console.log('RADI')
   }
 
   return (
-    <div className=" absolute sticky top-0 z-50 flex w-screen justify-between bg-red p-5 text-xl text-white">
+    <div className="sticky top-0 z-50 flex w-screen justify-between bg-red p-5 text-xl text-white">
       <div className="tracking-widest">došašće++</div>
       <GoogleLogin onSuccess={onSuccess} shape="circle" />
     </div>
