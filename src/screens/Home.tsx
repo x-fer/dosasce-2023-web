473c4d8af661789a831bf47ea5eccf5e9e99120e
Dominik Kanjuh <dominik.kanjuh@outlook.com>
2023-11-18 17:01:48 +0100

    Disable selecion of santa

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -7,10 +7,10 @@ const Home = () => {
       <div className="flex h-screen w-screen items-center justify-center bg-hero-pattern">
         <div className="flex flex-col justify-start gap-8 lg:flex-row lg:items-center lg:gap-36">
           <div className="flex h-[255px] w-[350px] items-center justify-center rounded-3xl bg-blue px-4 shadow-xl lg:h-[400px] lg:w-[550px] lg:rounded-[60px]">
-            <img src={santa} className="h-auto w-full" alt="Slika djeda božičnjaka" />
+            <img src={santa} className="h-auto w-full select-none" alt="Slika djeda božičnjaka" />
           </div>
 
-          <div className="font-monomaniac flex flex-col text-red">
+          <div className="flex flex-col font-monomaniac text-red">
             <p className="text-2xl">Dobrodošli na</p>
 
             <p className="text-7xl">došašće++</p>
