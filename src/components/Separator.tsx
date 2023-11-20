2afd1c5a7733017d42d07f2df0d628ffcc3d0b74
vlahovivan <ivan.vlahov@gmail.com>
2023-11-20 01:13:12 +0100

    Fix class attribute to className in Separator component

--- src/components/Separator.tsx
+++ src/components/Separator.tsx
@@ -1,5 +1,5 @@
 const Separator = () => {
-  return <div class="z-20 flex w-full justify-center bg-red p-2 font-bold text-white"></div>
+  return <div className="z-20 flex w-full justify-center bg-red p-2 font-bold text-white"></div>
 }
 
 export default Separator
