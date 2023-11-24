76d24db7af0fd094849bcabb03793849165e9206
Dominik Kanjuh <dominik.kanjuh@outlook.com>
2023-11-24 12:59:46 +0100

    404 page which looks oke

--- src/screens/ErrorPage.tsx
+++ src/screens/ErrorPage.tsx
@@ -1,5 +1,26 @@
+import { Background } from '@/components'
+
 const ErrorPage = () => {
-  return <div className="flex h-full w-full flex-grow items-center justify-center"> 404 Page not found</div>
+  return (
+    <Background>
+      <main className="flex h-full w-full flex-grow items-center justify-center  px-4 py-12 sm:py-16 lg:px-8">
+        <section className="flex h-full w-full items-center justify-center rounded-xl bg-white px-4">
+          <div className="text-center">
+            <p className="text-xl font-semibold text-red">404</p>
+            <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">Stranica ne postoji</h1>
+            <p className="paragraph mt-6 text-base leading-7 text-gray-600">
+              Nismo mogli pronaći stranicu koju tražite.
+            </p>
+            <div className="mt-10">
+              <a href="/" className="paragraph text-sm font-semibold leading-7 text-red">
+                <span aria-hidden="true">&larr;</span> Vratite se na početnu stranicu
+              </a>
+            </div>
+          </div>
+        </section>
+      </main>
+    </Background>
+  )
 }
 
 export default ErrorPage
