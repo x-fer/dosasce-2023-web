85302012e45bb141bf23a653f4be7d811dfcbd22
Dominik Kanjuh <dominik.kanjuh@outlook.com>
2023-11-24 13:05:27 +0100

    Small 404 changes to use Problem page layout so everything looks the same

--- src/components/ProblemPage.tsx
+++ src/components/ProblemPage.tsx
@@ -1,13 +1,13 @@
 import { ReactNode } from 'react'
 import { Background } from '@/components'
 
-const ProblemPage = ({ children, image }: { children: ReactNode; image: ReactNode }) => {
+const ProblemPage = ({ children, image }: { children: ReactNode; image?: ReactNode }) => {
   return (
     <Background>
       <div className="h-full w-full px-4 py-10 md:px-8 lg:px-10">
         <div className="flex h-full w-full flex-grow flex-col rounded-lg border-2 border-solid border-light-red bg-white">
           {image}
-          <section className="p-10">{children}</section>
+          <section className="h-full p-10">{children}</section>
         </div>
       </div>
     </Background>
--- src/screens/ErrorPage.tsx
+++ src/screens/ErrorPage.tsx
@@ -1,25 +1,19 @@
-import { Background } from '@/components'
+import ProblemPage from '@/components/ProblemPage'
 
 const ErrorPage = () => {
   return (
-    <Background>
-      <main className="flex h-full w-full flex-grow items-center justify-center  px-4 py-12 sm:py-16 lg:px-8">
-        <section className="flex h-full w-full items-center justify-center rounded-xl bg-white px-4">
-          <div className="text-center">
-            <p className="text-xl font-semibold text-red">404</p>
-            <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">Stranica ne postoji</h1>
-            <p className="paragraph mt-6 text-base leading-7 text-gray-600">
-              Nismo mogli pronaći stranicu koju tražite.
-            </p>
-            <div className="mt-10">
-              <a href="/" className="paragraph text-sm font-semibold leading-7 text-red">
-                <span aria-hidden="true">&larr;</span> Vratite se na početnu stranicu
-              </a>
-            </div>
-          </div>
-        </section>
-      </main>
-    </Background>
+    <ProblemPage>
+      <div className="flex h-full flex-col items-center justify-center text-center">
+        <p className="text-xl font-semibold text-red">404</p>
+        <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">Stranica ne postoji</h1>
+        <p className="paragraph mt-6 text-base leading-7 text-gray-600">Nismo mogli pronaći stranicu koju tražite.</p>
+        <div className="mt-10">
+          <a href="/" className="paragraph text-sm font-semibold leading-7 text-red">
+            <span aria-hidden="true">&larr;</span> Vratite se na početnu stranicu
+          </a>
+        </div>
+      </div>
+    </ProblemPage>
   )
 }
 
