be1cdfc54cead06394afd38b64a688a7eeb51235
KrunoT <krunoslav.tomicic@gmail.com>
2023-11-27 01:01:26 +0100

    fetched user data

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,46 @@ import Home from './screens/Home'
 import About from './screens/About'
 import ErrorPage from './screens/ErrorPage'
 import { Header, ProblemRouter } from './components'
+import React, { useState } from 'react'
+import axios from 'axios'
 
-function App() {
-  const token = localStorage.getItem('SavedLoginToken') || ''
+export const TokenContext = React.createContext(null)
+
+type VerifyTokenResponse = {
+  sub: string
+  hd: string
+  email: string
+  email_verified: string
+  name: string
+  picture: string
+}
+
+export const verifyToken = async (token: string) => {
+  const { data: niceGoogleResponse } = await axios.get<VerifyTokenResponse>('https://oauth2.googleapis.com/tokeninfo', {
+    params: { id_token: token },
+  })
+
+  if (niceGoogleResponse?.email_verified !== 'true') throw new Error('email not verified')
+
+  return niceGoogleResponse
+}
+
+export const extractUser = async (token: string) => {
+  const tokenData = await verifyToken(token).catch(() => null)
+
+  return tokenData
+}
+
+async function App() {
+  const [token, setToken] = useState(localStorage.getItem('SavedLoginToken') || '')
 
   if (token.length !== 0) {
     console.log('NAPOKON - GOSPODIN TOKEN RADI')
   }
+
+  console.log(extractUser(token))
+
+  setToken(token)
 
   return (
     <>
