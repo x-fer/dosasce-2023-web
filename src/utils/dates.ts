0bf5416225ddf58fe665f40af292d8ec7643d839
Dominik Kanjuh <dominik.kanjuh@outlook.com>
2023-11-28 11:16:20 +0100

    Added date on timeline

---
+++ src/utils/dates.ts
@@ -0,0 +1,23 @@
+interface problemDate {
+  startDate: Date
+  endDate?: Date
+}
+
+export const zad1: problemDate = {
+  startDate: new Date('2023-11-30'),
+  endDate: new Date('2023-12-6'),
+}
+
+export const zad2: problemDate = {
+  startDate: new Date('2023-12-7'),
+  endDate: new Date('2023-12-13'),
+}
+
+export const zad3: problemDate = {
+  startDate: new Date('2023-12-14'),
+  endDate: new Date('2023-12-20'),
+}
+
+export const dodjela: problemDate = {
+  startDate: new Date('2023-12-21'),
+}
