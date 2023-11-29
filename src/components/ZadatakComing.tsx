2bfcb8b1fa19038f8c70742e8dedc0c4d8c3e893
Dominik Kanjuh <dominik.kanjuh@outlook.com>
2023-11-29 20:00:56 +0100

    Coming text smaller on mobile for maja

--- src/components/ZadatakComing.tsx
+++ src/components/ZadatakComing.tsx
@@ -4,7 +4,7 @@ const ZadatakComing = ({ number }: { number: number }) => {
   const zad = getZad(number)
 
   return (
-    <div className="flex h-full w-full items-center justify-center text-center text-4xl">
+    <div className="flex h-full w-full items-center justify-center text-center text-2xl md:text-4xl">
       Ovaj zadatak će biti dostupan
       <br />
       od {zad.startDate.toLocaleDateString('hr', { day: 'numeric', month: 'numeric' })} do{' '}
--- src/screens/leaderboard/LeaderBoardComing.tsx
+++ src/screens/leaderboard/LeaderBoardComing.tsx
@@ -4,7 +4,7 @@ const LeaderBoardComing = ({ number }: { number: number }) => {
   const zad = getZad(number)
 
   return (
-    <div className="flex h-full w-full items-center justify-center text-center text-4xl">
+    <div className="flex h-full w-full items-center justify-center text-center text-2xl md:text-4xl">
       Ljestvica za {getZadatakDescription(number)}
       <br />
       biti će dostupna od {zad.startDate.toLocaleDateString('hr', { day: 'numeric', month: 'numeric' })}
