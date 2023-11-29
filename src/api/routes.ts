d3e9cc29618b1ec6258c8ad6f4e60a7f2fec32d4
Dominik Kanjuh <dominik.kanjuh@outlook.com>
2023-11-29 22:43:50 +0100

    Test for uzrast 3

--- src/api/routes.ts
+++ src/api/routes.ts
@@ -2,6 +2,6 @@ const kontestisURL = import.meta.env.VITE_API_ENDPOINT
 
 export const joinContest = () => `${kontestisURL}/api/contest/join`
 
-export const userCategoryEmail = (email: string) => `/user-category?email=${email}`
+export const userCategoryEmail = (email: string) => `/userCategory?email=${email}`
 
-export const userCategory = () => `/user-category`
+export const userCategory = () => `/userCategory`
