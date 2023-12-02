c56514b4026aaa5042f4bf6798a8d3122c8a8aed
Dominik Kanjuh <dominik.kanjuh@outlook.com>
2023-12-02 16:04:32 +0100

    drugi zad za branch building

--- src/screens/problems/SecondProblem.tsx
+++ src/screens/problems/SecondProblem.tsx
@@ -1,10 +1,22 @@
+import { Title } from '@/components'
+import ProblemPage from '@/components/ProblemPage'
 import ZadatakComing from '@/components/ZadatakComing'
 import { useZadActive } from '@/utils/dates'
 
 const SecondProblem = () => {
   const isZadActive = useZadActive(2)
 
-  return <>{isZadActive ? <div className="h-full flex-grow">SecondProblem</div> : <ZadatakComing number={2} />}</>
+  return (
+    <>
+      {isZadActive ? (
+        <ProblemPage>
+          <Title type="title">2. Zadatak</Title>
+        </ProblemPage>
+      ) : (
+        <ZadatakComing number={2} />
+      )}
+    </>
+  )
 }
 
 export default SecondProblem
