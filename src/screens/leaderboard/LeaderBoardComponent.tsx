e026ef6d40d467917d2ebfedf1da7d259800508b
Dominik Kanjuh <dominik.kanjuh@outlook.com>
2023-12-20 23:39:37 +0100

    add one console.log for the leaderboard

--- src/screens/leaderboard/LeaderBoardComponent.tsx
+++ src/screens/leaderboard/LeaderBoardComponent.tsx
@@ -124,6 +124,12 @@ const LeaderBoardComponent = ({ problemNumber }: { problemNumber: number }) => {
 
   const data = finalLeaderbaord[problemNumber as unknown as keyof typeof finalLeaderbaord]
 
+  let printData = []
+  if (leaderboardWithCategories?.length) {
+    printData = leaderboardWithCategories.map((el: any) => el.sort((a: any, b: any) => b.points - a.points)) //   data = leaderboardWithCategories.map((el: any) => el.sort((a: any, b: any) => b.points - a.points))
+    console.log('data', printData)
+  }
+
   return (
     <ProblemPage>
       <div className="p-5">
